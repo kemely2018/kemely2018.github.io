4edb5db36bf4ec983ccb166b4b70b40fc5632295
Kemely Castillo <41025582+kemely2018@users.noreply.github.com>
2023-09-19 05:40:39 -0500

    Add files via upload

---
+++ empatica-charts.js
@@ -0,0 +1,106 @@
+/* global day1, day2, day3, day4, day5, day6, day7, day8, Chart, createButton createDropdownRow */
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let day = 0;
+
+const days = [day1];
+
+const color = 'rgba(54, 162, 235)';
+
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    datasets: [{
+      data: days[day],
+      fill: false,
+      showLine: false,
+      lineTension: 0,
+      pointRadius: 1,
+      borderWidth: 2,
+      borderColor: color,
+      backgroundColor: color,
+      pointBorderColor: color,
+      pointBackgroundColor: color
+    }]
+  },
+  options: {
+    animation: {
+      duration: 0
+    },
+    hover: {
+      animationDuration: 0
+    },
+    legend: {
+      display: false
+    },
+    layout: {
+      padding: {
+        top: 50,
+        right: 20
+      }
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Fecha y hora'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Ritmo cardiaco (HR)'
+        }
+      }]
+    },
+    plugins: {
+      zoom: {
+        zoom: {
+          enabled: true
+        },
+        pan: {
+          enabled: true
+        }
+      }
+    }
+  }
+});
+
+const dropdownItems = [];
+for (const d in days) {
+  dropdownItems.push([d, `Day ${+d + 1} (${days[d].length} items)`]);
+}
+const firstButtonElements = ['info', 'changeView()', 'v', 'chart-line', '<u>V</u>iew: <span id="change-view">Point</span>'];
+const dropdownElements = [[['info dropdown-toggle', '', 'd', 'calendar-alt', `<u>D</u><span id="change-day-text">ay ${day + 1} (${days[day].length} items)</span>`], 'change-day', dropdownItems]];
+const secondButtonElements = ['info', 'chart.resetZoom()', 'r', 'undo-alt', '<u>R</u>eset zoom'];
+const dropdownRow = createDropdownRow(dropdownElements);
+dropdownRow.insertBefore(createButton(...firstButtonElements), dropdownRow.children[0]);
+dropdownRow.appendChild(createButton(...secondButtonElements));
+document.body.insertBefore(dropdownRow, canvas);
+document.querySelectorAll('.dropdown-item').forEach(e => {
+  e.addEventListener('click', function () {
+    document.getElementById('change-day-text').innerText = this.innerText.substring(1);
+    day = +this.dataset.bsValue;
+    chart.data.datasets[0].data = days[day];
+    chart.update();
+  });
+});
+
+window.changeView = function () {
+  if (chart.data.datasets[0].showLine) {
+    chart.data.datasets[0].showLine = false;
+    chart.data.datasets[0].pointRadius = 1;
+    document.getElementById('change-view').innerHTML = 'Point';
+  } else {
+    chart.data.datasets[0].showLine = true;
+    chart.data.datasets[0].pointRadius = 0;
+    document.getElementById('change-view').innerHTML = 'Line';
+  }
+  chart.update();
+};
---
+++ empatica-charts.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+from datetime import datetime, timedelta
+
+def read_heart_rate(file_name):
+  data = []
+  f = open(file_name, 'r')
+  dt = datetime.fromtimestamp(int(float(f.readline().rstrip())))
+  f.readline()
+  for x in f:
+    data.append([dt.strftime('%Y-%m-%d %H:%M:%S'), x.rstrip()])
+    dt += timedelta(seconds=1)
+  f.close()
+  return data
+
+days = []
+
+for e in sorted(os.listdir('data')):
+  if os.path.isdir(os.path.join('data', e)):
+    days += read_heart_rate(os.path.join('data', e, 'HR.csv'))
+
+date = days[0][0][0:10]
+day = 1
+f = open('./days.js', 'w+')
+fC = open('./daysC.js', 'w+')
+f.write('const day' + str(day) + ' = [')
+fC.write('const day' + str(day) + ' = [')
+for x in days:
+  if not x[0][0:10] == date:
+    date = x[0][0:10]
+    day += 1
+    f.write('];\n')
+    f.write('const day' + str(day) + ' = [')
+  f.write('{ x: new Date("%s"), y: %s },' % (x[0], x[1]))
+  fC.write( x[1])
+
+f.write('];\n')
+fC.write(']\n')
+f.close()
+fC.close()
